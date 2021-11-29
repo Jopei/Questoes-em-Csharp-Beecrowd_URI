806c98647d8ee1ca4f1d5f9cafb2b2abe190c820
Jopei <joaomoura.269@gmail.com>
2021-11-28 23:34:07 -0300

    1234 funcional

--- 1234/Program.cs
+++ 1234/Program.cs
@@ -8,82 +8,34 @@ namespace _1234
 {
     internal class Program
     {
-        void Fprint(int cont,string result)
-        {
-
-            for (int i = 0; i < cont; i++)
-            {
-
-                Console.Write(result[i]);
-                Console.WriteLine();
-
-            }
-        }
-        private static void Fprint(int cont, List<string> result)
-        {
-            throw new NotImplementedException();
-        }
+        
         static void Main(string[] args)
         {
-            int length;
+            
+            bool upper = true;
             string sentence;
-            
-            int cont = 0;
-            //sentence = Console.ReadLine();
-            List<string> result = new List<string> { };
-            while (!string.IsNullOrWhiteSpace(sentence = Console.ReadLine()))
+            sentence = Console.ReadLine();
+            StringBuilder sentenceUpper = new StringBuilder(sentence);
+            for (int i = 0; i < sentence.Length; i++)
             {
-
-                //StringBuilder sentenceUpper = new StringBuilder(sentence);
-                char[] sentenceChar = sentence.ToCharArray();
-                bool upper = true;
-                string sentenceUpper = "";
-                length = sentence.Length;
-                for (int i = 0; i < length; i++)
+                if(sentence[i] == ' ')
                 {
-                    /*if (sentence[i] == ' ')
-                    {
-                        sentenceUpper += " ";
-
-                    }
-                    else if (upper)
-                    {
-                        sentenceUpper += char.ToUpper(sentenceChar[i]); 
-                        upper = false;
-
-                    }
-                    else
-                    {
-                        sentenceUpper += char.ToLower(sentenceChar[i]);
-                        upper = true;
-                    }*/
-                    if (sentenceChar[i] != ' ')
-                    {
-                        if (upper)
-                        {
-
-                            sentenceUpper += char.ToUpper(sentenceChar[i]);
-                        }
-
-                        else
-                        {
-                            sentenceUpper += char.ToLower(sentenceChar[i]);
-                        }
-
-                        upper = !upper;
-                    }
-
-                    else
-                    {
-                        sentenceUpper += " ";
-                    }
+                    sentenceUpper[i] = sentence[i];
                 }
-                result.Insert(cont, sentenceUpper);
-                cont++;
+                else if (upper)
+                {
+                    sentenceUpper[i] = sentence.ToUpper()[i];
+                    upper = false;
+
+                }
+                else
+                {
+                    sentenceUpper[i] = sentence.ToLower()[i];
+                    upper = true;
+                }
             }
-            Fprint(cont, result);
-            
-            //Console.WriteLine(final);
+            sentence = sentenceUpper.ToString();
+            Console.WriteLine(sentence);
             Console.ReadLine();
 
         }
